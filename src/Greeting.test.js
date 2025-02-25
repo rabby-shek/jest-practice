b49b46510fa29cc657a6c19f007da12093a7094d
rabby-shek <rabbyshek1998@gmail.com>
2025-02-25 12:19:35 +0600

    test grouping learning complete

--- src/Greeting.test.js
+++ src/Greeting.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greetings";
+describe("Greetings component Testing", () => {
+  test("Render with default text", () => {
+    render(<Greetings />);
+    const defaultGreetingsText = screen.getByText(/Hello User/i);
+    expect(defaultGreetingsText).toBeInTheDocument();
+  });
 
-test("Render with default text", () => {
-  render(<Greetings />);
-  const defaultGreetingsText = screen.getByText(/Hello User/i);
-  expect(defaultGreetingsText).toBeInTheDocument();
-});
-
-test("Render with custom text", () => {
-  render(<Greetings name="Suvo" />);
-  const customGreetingsText = screen.getByText("Hello Suvo");
-  expect(customGreetingsText).toBeInTheDocument();
+  test("Render with custom text", () => {
+    render(<Greetings name="Suvoo" />);
+    const customGreetingsText = screen.getByText("Hello Suvoo");
+    expect(customGreetingsText).toBeInTheDocument();
+  });
 });
