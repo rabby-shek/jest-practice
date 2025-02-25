14ffab4ecf1234eebd90b27cdc702e594700623c
rabby-shek <rabbyshek1998@gmail.com>
2025-02-25 13:37:08 +0600

    learning about getting the values from the virtual dom

--- src/getName.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-
-test('render the submit text', () => {
-    render(<div>Submit</div>);
-    const submit = screen.getByText(/Submit/i);
-    expect(submit).toBeInTheDocument();
-});
